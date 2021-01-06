b86101ca3051109c43b737aa07ed53c4fb382150
Federico Rinaldi <gisquerin@gmail.com>
2021-01-06 13:26:16 +0100

    Add ShapeSprite trait with Rectangle as example

--- src/basic_shapes.rs
+++ src/basic_shapes.rs
@@ -1,11 +1,12 @@
 //! Common shapes, like rectangles, ellipses, triangles and more.
 
-use crate::{create_sprite, Geometry, TessellationMode};
+use crate::{create_sprite, Geometry, ShapeSprite, TessellationMode};
 use bevy::prelude::*;
 use lyon_tessellation::{
     math::{Point, Rect, Size},
     path::{Polygon, Winding},
-    BuffersBuilder, FillTessellator, FillVertex, StrokeTessellator, StrokeVertex, VertexBuffers,
+    BuffersBuilder, FillOptions, FillTessellator, FillVertex, StrokeTessellator, StrokeVertex,
+    VertexBuffers,
 };
 
 /// Basic shapes descriptors, used in [`primitive`](primitive).
@@ -14,6 +15,50 @@ pub enum ShapeType {
     Circle(f32),
     Ellipse { radius_x: f32, radius_y: f32 },
     Polygon { points: Vec<Point>, closed: bool },
+}
+
+pub enum RectangleOrigin {
+    BottomLeft,
+    BottomRight,
+    TopRight,
+    TopLeft,
+    Center,
+}
+
+pub struct Rectangle {
+    pub width: f32,
+    pub height: f32,
+    //pub origin: RectangleOrigin,
+}
+
+impl ShapeSprite for Rectangle {
+    fn fill(
+        &self,
+        material: Handle<ColorMaterial>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        tessellator: &mut FillTessellator,
+        transform: Transform,
+        fill_options: &FillOptions,
+    ) -> SpriteBundle {
+        let mut geometry = Geometry(VertexBuffers::new());
+        let ref mut output = BuffersBuilder::new(&mut geometry.0, |vertex: FillVertex| {
+            [vertex.position().x, vertex.position().y, 0.0]
+        });
+
+        tessellator
+            .tessellate_rectangle(
+                &Rect::new(Point::zero(), Size::new(self.width, self.height)),
+                fill_options,
+                output,
+            )
+            .unwrap();
+
+        create_sprite(material, meshes, geometry, transform.translation)
+    }
+
+    /* fn stroke(stroke_options: &StrokeOptions) -> SpriteBundle {
+        todo!()
+    } */
 }
 
 /// Returns a `SpriteBundle` bundle using the given [`ShapeType`](ShapeType)

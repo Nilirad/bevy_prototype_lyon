1d916ecab13619aeb11fd108d126055a7b3f056e
Federico Rinaldi <gisquerin@gmail.com>
2021-01-06 15:43:39 +0100

    Add Ellipse ShapeSprite

--- src/basic_shapes.rs
+++ src/basic_shapes.rs
@@ -1,12 +1,12 @@
 //! Common shapes, like rectangles, ellipses, triangles and more.
 
 use crate::{
-    conversions::BevyVec2ToLyonPoint, create_sprite, Geometry, ShapeSprite, TessellationMode,
-    Tessellator,
+    conversions::{ToLyonPoint, ToLyonVector},
+    create_sprite, Geometry, ShapeSprite, TessellationMode, Tessellator,
 };
 use bevy::prelude::*;
 use lyon_tessellation::{
-    math::{Point, Rect, Size},
+    math::{Angle, Point, Rect, Size},
     path::{Polygon, Winding},
     BuffersBuilder, FillTessellator, FillVertex, StrokeTessellator, StrokeVertex, VertexBuffers,
 };
@@ -161,6 +161,78 @@ impl ShapeSprite for Circle {
                     .as_mut()
                     .unwrap()
                     .tessellate_circle(self.center.to_lyon_point(), self.radius, options, output)
+                    .unwrap();
+            }
+        }
+
+        create_sprite(material, meshes, geometry, transform.translation)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Ellipse {
+    pub radii: Vec2,
+    /// The position of the center of the ellipse, relative to the world
+    /// [`Translation`] of the [`SpriteBundle`].
+    pub center: Vec2,
+}
+
+impl Default for Ellipse {
+    fn default() -> Self {
+        Self {
+            radii: Vec2::one(),
+            center: Vec2::zero(),
+        }
+    }
+}
+
+impl ShapeSprite for Ellipse {
+    fn generate_sprite(
+        &self,
+        material: Handle<ColorMaterial>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        tessellator: &mut Tessellator,
+        mode: &TessellationMode,
+        transform: Transform,
+    ) -> SpriteBundle {
+        let mut geometry = Geometry(VertexBuffers::new());
+
+        match mode {
+            TessellationMode::Fill(options) => {
+                let ref mut output = BuffersBuilder::new(&mut geometry.0, |vertex: FillVertex| {
+                    [vertex.position().x, vertex.position().y, 0.0]
+                });
+                tessellator
+                    .fill
+                    .as_mut()
+                    .unwrap()
+                    .tessellate_ellipse(
+                        self.center.to_lyon_point(),
+                        self.radii.to_lyon_vector(),
+                        Angle::zero(),
+                        Winding::Positive,
+                        options,
+                        output,
+                    )
+                    .unwrap();
+            }
+            TessellationMode::Stroke(options) => {
+                let ref mut output =
+                    BuffersBuilder::new(&mut geometry.0, |vertex: StrokeVertex| {
+                        [vertex.position().x, vertex.position().y, 0.0]
+                    });
+                tessellator
+                    .stroke
+                    .as_mut()
+                    .unwrap()
+                    .tessellate_ellipse(
+                        self.center.to_lyon_point(),
+                        self.radii.to_lyon_vector(),
+                        Angle::zero(),
+                        Winding::Positive,
+                        options,
+                        output,
+                    )
                     .unwrap();
             }
         }
--- src/conversions.rs
+++ src/conversions.rs
@@ -1,13 +1,23 @@
 //! Conversions between `bevy` and `lyon` datatypes.
 use bevy::math::Vec2;
-use lyon_tessellation::math::Point;
+use lyon_tessellation::math::{Point, Vector};
 
-pub trait BevyVec2ToLyonPoint {
+pub trait ToLyonPoint {
     fn to_lyon_point(self) -> Point;
 }
 
-impl BevyVec2ToLyonPoint for Vec2 {
+pub trait ToLyonVector {
+    fn to_lyon_vector(self) -> Vector;
+}
+
+impl ToLyonPoint for Vec2 {
     fn to_lyon_point(self) -> Point {
         Point::new(self.x, self.y)
+    }
+}
+
+impl ToLyonVector for Vec2 {
+    fn to_lyon_vector(self) -> Vector {
+        Vector::new(self.x, self.y)
     }
 }

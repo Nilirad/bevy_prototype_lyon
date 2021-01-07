3660b8f978b29bd8d9c313746884827cfb0e8c5f
Federico Rinaldi <gisquerin@gmail.com>
2021-01-07 16:48:08 +0100

    Add ShapePlugin

--- examples/plugin.rs
+++ examples/plugin.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
+#[bevy_main]
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .add_startup_system(startup.system())
+        .run();
+}
+
+fn startup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let material = materials.add(ColorMaterial::color(Color::CRIMSON));
+
+    let shape = CircleShape {
+        radius: 100.0,
+        ..Default::default()
+    };
+
+    commands.spawn(Camera2dBundle::default()).spawn(shape.draw(
+        material,
+        TessellationMode::Fill(FillOptions::default()),
+        Transform::default(),
+    ));
+}

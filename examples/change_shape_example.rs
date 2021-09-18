e182c9fba185fafd6897e7788e0531281b3fa01e
Federico Rinaldi <gisquerin@gmail.com>
2021-09-18 15:43:18 +0200

    Add path change to change example

--- examples/change_shape_example.rs
+++ examples/change_shape_example.rs
@@ -8,6 +8,7 @@ fn main() {
         .add_plugin(ShapePlugin)
         .add_startup_system(setup_system)
         .add_system(change_draw_mode_system)
+        .add_system(change_number_of_sides)
         .add_system(rotate_shape_system)
         .run();
 }
@@ -15,18 +16,18 @@ fn main() {
 struct ExampleShape;
 
 fn rotate_shape_system(mut query: Query<&mut Transform, With<ExampleShape>>, time: Res<Time>) {
-    for mut transform in query.iter_mut() {
-        let delta = time.delta_seconds();
+    let delta = time.delta_seconds();
 
+    for mut transform in query.iter_mut() {
         transform.rotate(Quat::from_rotation_z(0.2 * delta));
     }
 }
 
 fn change_draw_mode_system(mut query: Query<&mut DrawMode>, time: Res<Time>) {
-    for mut draw_mode in query.iter_mut() {
-        let hue = (time.seconds_since_startup() * 50.0) % 360.0;
-        let outline_width = 2.0 + time.seconds_since_startup().sin().abs() * 10.0;
+    let hue = (time.seconds_since_startup() * 50.0) % 360.0;
+    let outline_width = 2.0 + time.seconds_since_startup().sin().abs() * 10.0;
 
+    for mut draw_mode in query.iter_mut() {
         if let DrawMode::Outlined {
             ref mut fill_mode,
             ref mut outline_mode,
@@ -35,6 +36,20 @@ fn change_draw_mode_system(mut query: Query<&mut DrawMode>, time: Res<Time>) {
             fill_mode.color = Color::hsl(hue as f32, 1.0, 0.5);
             outline_mode.options.line_width = outline_width as f32;
         }
+    }
+}
+
+fn change_number_of_sides(mut query: Query<&mut Path>, time: Res<Time>) {
+    let sides = (time.seconds_since_startup().sin() * 2.5 + 5.5).round() as usize;
+
+    for mut path in query.iter_mut() {
+        let polygon = shapes::RegularPolygon {
+            sides,
+            feature: shapes::RegularPolygonFeature::Radius(200.0),
+            ..shapes::RegularPolygon::default()
+        };
+
+        *path = ShapePath::build_as(&polygon);
     }
 }
 

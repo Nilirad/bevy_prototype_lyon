fecefd5c284d068d0f83d3b095a7522e59234acd
Rob Parrett <robparrett@gmail.com>
2022-01-02 08:52:02 -0700

    Move shader into separate file

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -129,48 +129,6 @@ type DrawColoredMesh2d = (
     DrawMesh2d,
 );
 
-// The custom shader can be inline like here, included from another file at build time
-// using `include_str!()`, or loaded like any other asset with `asset_server.load()`.
-const COLORED_MESH2D_SHADER: &str = r"
-// Import the standard 2d mesh uniforms and set their bind groups
-#import bevy_sprite::mesh2d_view_bind_group
-[[group(0), binding(0)]]
-var<uniform> view: View;
-#import bevy_sprite::mesh2d_struct
-[[group(1), binding(0)]]
-var<uniform> mesh: Mesh2d;
-// The structure of the vertex buffer is as specified in `specialize()`
-struct Vertex {
-    [[location(0)]] position: vec3<f32>;
-    [[location(1)]] color: vec4<f32>;
-};
-struct VertexOutput {
-    // The vertex shader must set the on-screen position of the vertex
-    [[builtin(position)]] clip_position: vec4<f32>;
-    // We pass the vertex color to the framgent shader in location 0
-    [[location(0)]] color: vec4<f32>;
-};
-/// Entry point for the vertex shader
-[[stage(vertex)]]
-fn vertex(vertex: Vertex) -> VertexOutput {
-    var out: VertexOutput;
-    // Project the world position of the mesh into screen position
-    out.clip_position = view.view_proj * mesh.model * vec4<f32>(vertex.position, 1.0);
-    out.color = vertex.color;
-    return out;
-}
-// The input of the fragment shader must correspond to the output of the vertex shader for all `location`s
-struct FragmentInput {
-    // The color is interpolated between vertices by default
-    [[location(0)]] color: vec4<f32>;
-};
-/// Entry point for the fragment shader
-[[stage(fragment)]]
-fn fragment(in: FragmentInput) -> [[location(0)]] vec4<f32> {
-    return in.color;
-}
-";
-
 /// Plugin that renders [`ColoredMesh2d`]s
 pub struct ColoredMesh2dPlugin;
 
@@ -184,7 +142,7 @@ impl Plugin for ColoredMesh2dPlugin {
         let mut shaders = app.world.get_resource_mut::<Assets<Shader>>().unwrap();
         shaders.set_untracked(
             COLORED_MESH2D_SHADER_HANDLE,
-            Shader::from_wgsl(COLORED_MESH2D_SHADER),
+            Shader::from_wgsl(include_str!("colored_mesh2d.wgsl")),
         );
 
         // Register our custom draw function and pipeline, and add our render systems

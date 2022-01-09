ee83805e8de6b7555a6452913e7d26af5bdc7bc2
Nilirad <gisquerin@gmail.com>
2022-01-09 13:00:45 +0100

    Add path example

---
+++ examples/path.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let mut path_builder = PathBuilder::new();
+    path_builder.move_to(Vec2::ZERO);
+    path_builder.line_to(100.0 * Vec2::ONE);
+    let line = path_builder.build();
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(GeometryBuilder::build_as(
+        &line,
+        DrawMode::Stroke(StrokeMode::new(Color::BLACK, 10.0)),
+        Transform::default(),
+    ));
+}

77999acfdc240140cabb09204eefad6874e6f1b7
Federico Rinaldi <gisquerin@gmail.com>
2023-09-10 18:48:17 +0200

    Use `MaterialMesh2dBundle` with `ColorMaterial` (#224)
    * Use `MaterialMesh2dBundle` with `ColorMaterial`
    
    * clippy
    
    * Fix green channel filtering bug
    
    * Clippy again

--- src/entity.rs
+++ src/entity.rs
@@ -1,22 +1,29 @@
 //! Custom Bevy ECS bundle for shapes.
 
-use bevy::{
-    ecs::{bundle::Bundle, component::Component},
-    prelude::{Handle, SpatialBundle},
-    sprite::Mesh2dHandle,
-};
+use bevy::{prelude::*, sprite::Mesh2dHandle};
 use lyon_tessellation::{self as tess};
 
-use crate::{prelude::Geometry, render::ShapeMaterial};
+use crate::{geometry::Geometry, plugin::COLOR_MATERIAL_HANDLE};
 
 /// A Bevy `Bundle` to represent a shape.
 #[allow(missing_docs)]
-#[derive(Bundle, Default)]
+#[derive(Bundle)]
 pub struct ShapeBundle {
     pub path: Path,
     pub mesh: Mesh2dHandle,
-    pub material: Handle<ShapeMaterial>,
+    pub material: Handle<ColorMaterial>,
     pub spatial: SpatialBundle,
+}
+
+impl Default for ShapeBundle {
+    fn default() -> Self {
+        Self {
+            path: default(),
+            mesh: default(),
+            material: COLOR_MATERIAL_HANDLE,
+            spatial: default(),
+        }
+    }
 }
 
 #[allow(missing_docs)]
